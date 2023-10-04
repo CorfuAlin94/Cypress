63eeaf87772c3dfeebffa9baa82b2cd6efae2bb5
CorfuAlin94 <corfualinconstantin@gmail.com>
2023-10-04 16:30:44 +0300

    test

--- cypress.config.js
+++ cypress.config.js
@@ -4,10 +4,10 @@ module.exports = defineConfig({
   viewportWidth: 1024,
   viewportHeight: 768,
   chromeWebSecurity: false,
-  projectId: "fotqzd",
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
   },
+  projectId: "n7jmem",
 });
