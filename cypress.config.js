ba5523ea0ee16f47d42d7d357890bdb10460e3b3
CorfuAlin94 <corfualinconstantin@gmail.com>
2023-10-05 11:06:40 +0300

    05.10.2023

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  reporter: "mochawesome",
   viewportWidth: 1024,
   viewportHeight: 768,
   chromeWebSecurity: false,
